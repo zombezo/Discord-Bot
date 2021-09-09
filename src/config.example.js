da338f8ca20d956e47f921477e80704e9dab3abe
zombezo <90417257+zombezo@users.noreply.github.com>
2021-09-09 21:08:47 +0200

    Update config.example.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,6 @@
 const config = {
-	ownerID: ['YourAccountID'],
-	token: 'YourBotToken',
+	ownerID: ['774229026797518869'],
+	token: ''ODg0NzQ1MTc4MDMwNjc4MDE3.YTc9KA.cbMJ4tO6Lnf1OJUd6C2SgAOwjsQ,
 	// For looking up Twitch, Fortnite, Steam accounts
 	api_keys: {
 		// https://dev.twitch.tv/console/apps
@@ -14,8 +14,8 @@ const config = {
 		steam: 'steamAPI-Key',
 		// https://developer.spotify.com/documentation/web-api/
 		spotify: {
-			iD: 'spotify-ID',
-			secret: 'spotify-secret',
+			iD: 'b1fecfa4de574ad1bb9728e3e614a967',
+			secret: '1dee6c324ad248988aab239ff778c597',
 		},
 		// Your Ubisoft email and password (You don't need to enable anything)
 		rainbow: {
@@ -34,18 +34,18 @@ const config = {
 	// your support server
 	SupportServer: {
 		// Link to your support server
-		link: 'https://discord.gg/8g6zUQu',
+		link: 'https://discord.gg/mCVhGWTWCr',
 		// Your support's server ID
-		GuildID: '750822670505082971',
+		GuildID: '877933607291867136',
 		// This for using the suggestion command on your server
-		ModRole: '751857618720522341',
+		ModRole: '884521934941982782',
 		// What channel to post the suggestions
-		SuggestionChannel: '761619652009787392',
+		SuggestionChannel: '877938203749912627',
 		// Where the bot will send Guild join/leave messages to
-		GuildChannel: '761619652009787392',
+		GuildChannel: '877936261233512449',
 	},
 	// URL to mongodb
-	MongoDBURl: 'mongodb://link',
+	MongoDBURl: 'mongodb://https://cloud.mongodb.com/v2/613a553606a4592cc9bcaee4#host/replicaSet/613a5613cdd05d42ccc00281',
 	// embed colour
 	embedColor: 'RANDOM',
 	// This will spam your console if you enable this but will help with bug fixing
